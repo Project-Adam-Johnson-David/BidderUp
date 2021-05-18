1d27a9f0c997d39d317cc6f0966ab28acb8cfb6b
adam4sale <adambosch8@gmail.com>
2021-05-18 17:55:38 -0500

    Hell

--- BackEnd/src/main/java/com/revature/Project2/Project2Application.java
+++ BackEnd/src/main/java/com/revature/Project2/Project2Application.java
@@ -1,25 +1,40 @@
 package com.revature.Project2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 public class Project2Application {
 
+	@Autowired
+	private MongoTemplate mongo;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) throws Exception {
 		return args -> {
+<<<<<<< HEAD:src/main/java/com/revature/Project2/Project2Application.java
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			//Tells me that the Mongo has mad a connection to the test db
+			System.out.println("---------->" + mongo.getCollectionNames());//returns test collection
+=======
 //
 //			System.out.println("Let's inspect the beans provided by Spring Boot:");
+>>>>>>> 87e9d523e72265df514e1cfa63657e3354900df5:BackEnd/src/main/java/com/revature/Project2/Project2Application.java
 		};
 	}
 
---
+++ BackEnd/src/main/java/com/revature/Project2/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.revature.Project2.controller;
+
+import com.revature.Project2.pojo.User;
+import com.revature.Project2.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public String welcomeUser(){
+        return "Welcome to UserController";
+    }
+
+    /**
+     * Get method getUsers()
+     * endpoint '/users'
+     * @return List<User> list
+     */
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        List<User> list = new ArrayList<>();
+        try{
+            list = service.findAllUsers();
+        }
+        catch (Exception e){
+            //System.out.println(e.getMessage());
+            e.printStackTrace();
+            list.add(new User(null, "Adam", "password",
+                    "United States", 100));
+        }
+        return list;
+    }
+
+}
