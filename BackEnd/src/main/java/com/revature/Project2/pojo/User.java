fa6f8d47b211ed02df7230c7e6b5f500e4773019
adam4sale <adambosch8@gmail.com>
2021-05-19 18:04:43 -0500

    f github

--- BackEnd/src/main/java/com/revature/Project2/pojo/User.java
+++ BackEnd/src/main/java/com/revature/Project2/pojo/User.java
@@ -1,6 +1,7 @@
 package com.revature.Project2.pojo;
 
 import lombok.*;
+import lombok.Getter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
