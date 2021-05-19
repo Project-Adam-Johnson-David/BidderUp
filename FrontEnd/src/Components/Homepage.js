5795596239fd3948cd58c3d1a6b9c9cd40d2312f
David Jasuan <davidjasuan@Davids-MacBook-Pro.local>
2021-05-18 20:06:40 -0700

    huh?

---
+++ FrontEnd/src/Components/Homepage.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import axios from "axios";
+
+function Homepage(props){
+
+    return (
+
+        <div>
+asfsadfasdf
+        </div>
+
+    );
+}
+
+export default Homepage;
