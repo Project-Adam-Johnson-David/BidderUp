04eddaa7ba6bb9901d8ae6a21f396de57f80fe8e
johnniboi97 <johnniboi97@gmail.com>
2021-05-26 00:21:58 -0500

    refactored front end login

--- FrontEnd/src/Components/TestLogin.js
+++ FrontEnd/src/Components/TestLogin.js
@@ -9,72 +9,29 @@ import {useHistory} from 'react-router-dom'
 function TestLogin(props){
     const [username, setUsername]= useState(localStorage.getItem("username") || "");
     const [password, setPassword]= useState("");
-    const [loggedIn, setLoggedIn]= useState(localStorage.getItem("loggedIn")||false);
-    const [page, setPage]=useState("login");
     const history = useHistory() 
-    console.log(loggedIn)
-
-    React.useEffect(() => {
-        let username1 = localStorage.getItem("username")
-        console.log(username1+ "username??");
-        // //currUser
-        setUsername("")
-        if(username1!==""){
-            setUsername(username1)
-            console.log("code was here");
-        }
-        //
-        // logged in status
-        let loggedStatus = (localStorage.getItem("loggedIn")=== 'true');
-        setLoggedIn(loggedStatus)
-    }, [])
-
-    React.useEffect(()=>{
-        console.log("should be saved to local"+username);
-        localStorage.setItem("username", username);
-        localStorage.setItem("loggedIn",loggedIn)
-        // setPage("login");
-    },[loggedIn, username]);
-
-    function logOut(){
-        console.log("code was here at logout");
-        console.log("logged in was set false here");
-        localStorage.setItem("loggedIn","false");
-        setLoggedIn(false);
-        setUsername("");
-    }
 
     async function submitForm(e){
         e.preventDefault();
         console.log(username);
         console.log(password);
 
-        let id = null;
-        let country = "country";
-        let balance = 4;
         await axios({
             method: 'post',
             url: "http://localhost:8080/user/login",
-            data: { username, password, id, country,balance},
+            data: { username, password},
             // params:{username, password},
             headers : {
                 'Content-Type': 'application/json'
             }
         }).then(res => {
-            let status = res.data;
-            if(status===200){
-                NotificationManager.success('Successful', 'Successfully logged in!');
-                setLoggedIn(true);
-                localStorage.setItem("username", username);
-                sessionStorage.setItem("username", username);
-                history.push('/HomePage', {username: username}, logOut)
-                console.log(res.status);
-            }
-            else{
-                NotificationManager.error('Unsuccessful', 'Sorry, we couldn\'t login with the provided credentials');
-                // console.log("response was not 200"+res.data);
-            }
-        })
+            NotificationManager.success('Successful', 'Successfully logged in!');
+            localStorage.setItem("username", username);
+            sessionStorage.setItem("username", username);
+            sessionStorage.setItem("country", res.data.country);
+            history.push('/HomePage')
+            console.log(res.status);
+            })
             .catch(err => alert(err));
     }
 
