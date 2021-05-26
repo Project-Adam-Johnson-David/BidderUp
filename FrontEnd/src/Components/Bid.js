da94014bf170385afad3ca5eae374f1211648f26
adam4sale <adambosch8@gmail.com>
2021-05-26 00:13:34 -0500

    bid and bids

--- FrontEnd/src/Components/Bid.js
+++ FrontEnd/src/Components/Bid.js
@@ -1,20 +1,49 @@
-import image from '../images.jpeg'
+import React, {useState} from 'react';
+import axios from "axios";
 function Bid(props){
-    return(
+    const [bids, setBids] = useState([]);
+    React.useEffect(()=>{
+        getBidsByItemName();
+    },[]);
 
-        <div className="bid">
-            <h6>New York Cosmos Jersey</h6>
-            <div><img className="item-image" src={image}/></div>
-            <div>
-                <p>Title</p>
-                <p>Price</p>
-                <p>Date submitted</p>
-                <p>Date posted</p>
+        async function getBidsByItemName(){
+        let item= props.item;
+        let itemName = item.title;
+        let owner = item.owner;
+            await axios({
+                method: 'get',
+                url: `http://localhost:8080/bid/bids/${owner}/${itemName}`,
+                headers : {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => {
+            let result = res.data;
+            setBids(result);
+               //setBids(res.data);
+               //bids = res.data;
+               console.log(bids);
+            })
+                .catch(err => alert(err));
+        }
+
+
+
+    return(
+     {bids.map(b => {
+        return(
+            <div className="bid" key={b.id}>
+                <div>
+                    <p>Bidder: {b.bidder}</p>
+                    <p>Price: {b.amount}</p>
+                    <p>Date submitted: {b.date}</p>
+                </div>
+                <button className="accept">accept</button>
+                <button  className="deny">deny</button>
             </div>
-            <button className="accept">accept</button>
-            <button  className="deny">deny</button>
-        </div>
-    );
+        )
+     })}
+     <div>hello werldd</div>
+   )
 }
 
 export default Bid;
