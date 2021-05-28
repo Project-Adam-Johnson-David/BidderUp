77b05489cb3e424df327ba50c021ce1a0f9b1277
johnniboi97 <johnniboi97@gmail.com>
2021-05-28 00:17:09 -0500

    added logging to controller classes

--- BackEnd/src/main/java/com/revature/Project2/controller/BidController.java
+++ BackEnd/src/main/java/com/revature/Project2/controller/BidController.java
@@ -4,6 +4,7 @@ import com.revature.Project2.pojo.Bid;
 import com.revature.Project2.repository.BidRepository;
 import com.revature.Project2.service.BidService;
 import com.revature.Project2.service.ItemService;
+import lombok.extern.log4j.Log4j2;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.CreatedDate;
@@ -17,6 +18,7 @@ import java.util.Random;
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/bid")
+@Log4j2
 public class BidController {
     //vars
     @Autowired
@@ -33,18 +35,21 @@ public class BidController {
      */
     @GetMapping("/bids")
     public List<Bid> getBids(){
+        log.info("Finding all bids in controller");
         List<Bid> list = repo.findAll();
         return list;
     }
 
     @GetMapping("/bids/{owner}/{itemName}")
     public ArrayList<Bid> getItemBids(@PathVariable("owner") String owner, @PathVariable("itemName") String item){
+        log.info("finding bids by item in controller");
         ArrayList<Bid> bids = service.findBidByItem(owner, item);
         return bids;
     }
 
     @GetMapping("/bids/{username}")
     public ArrayList<Bid> getBidsById(@PathVariable("username") String username){
+        log.info("finding bids by ID in controller");
         ArrayList result =service.findBidByOwner(username);
         System.out.println(result);
         return result;
@@ -52,6 +57,7 @@ public class BidController {
 
     @GetMapping("/accepted/{username}")
     public ArrayList<Bid> getAcceptedBids(@PathVariable("username") String username){
+        log.info("getting accepted bids in controller");
         ArrayList result =service.findAcceptedBidsByBidder(username, "accept");
         System.out.println(result);
         return result;
@@ -59,9 +65,11 @@ public class BidController {
 
     @PostMapping(value ="/post_bid")
     public boolean postBid(@RequestBody Bid bid){
+        log.info("request to post a bid in controller");
         return service.postBid(bid);
     }
 
     @PostMapping(value="/bid_status/{value}")
     public String bidStatus(@RequestBody String id, @PathVariable("value") String status){
+        log.info("setting status of bid in controller");
         return service.setBidStatus(id, status); }}
--- BackEnd/src/main/java/com/revature/Project2/controller/ItemController.java
+++ BackEnd/src/main/java/com/revature/Project2/controller/ItemController.java
@@ -4,6 +4,7 @@ import com.revature.Project2.pojo.Item;
 import com.revature.Project2.pojo.User;
 import com.revature.Project2.repository.ItemRepository;
 import com.revature.Project2.service.ItemService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/item")
+@Log4j2
 public class ItemController {
     //vars
     @Autowired
@@ -27,7 +29,7 @@ public class ItemController {
     @PostMapping("/addItem")
     public String addItem(@RequestBody Item item) {
         //item
-
+        log.info("adding item in controller");
         String status = "error";
         item.setAccepted(false);
         if(service.insertItem(item)){status = "200";}
@@ -42,7 +44,7 @@ public class ItemController {
      */
     @GetMapping("/owner_items/{owner}")
     public List<Item> getOwnerItems(@PathVariable("owner") String owner){
-
+        log.info("getting owners items in controller");
         return service.findOwnerItems(owner);
     }
 
@@ -56,17 +58,20 @@ public class ItemController {
     @GetMapping("/owner_items/{owner}/{title}")
     public Item getOwnerItem(@PathVariable("owner") String owner,
                              @PathVariable("title") String title){
+        log.info("finding the owner of an item in controller");
         return service.findOwnerItem(owner, title);
     }
 
     @GetMapping("/browse/{query}/{username}")
     public ArrayList<Item> browseItems(@PathVariable("query") String query, @PathVariable("username") String username){
+        log.info("searching for items in controller");
         ArrayList data= new ArrayList();
         try{ data= service.findItems(query, username); } catch (Exception e){ e.printStackTrace();
         }return data; }
 
     @GetMapping("/items/accepted/{owner}")
     public List<Item> getAcceptedItems(@PathVariable("owner") String owner){
+        log.info("getting accepted items in controller");
         return service.findAcceptedOwnerItems(owner); }
 
 }
